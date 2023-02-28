cbb3dad950dc105567cd75cb1ce618e0a03ee4be
Adebobola <adepojuadebobola6@gmail.com>
2023-02-28 13:13:19 +0100

    functions of TODO

--- index.js
+++ index.js
@@ -1,38 +1,71 @@
 'use strict';
-const writeTodo = document.querySelector(".write");
+
 const screen = document.querySelector(".screen");
-let  writeAll = document.querySelectorAll(".writeOne");
-// const write = document.querySelector(".writeOne");
+const write = document.querySelector(".write");
+const written = document.querySelectorAll(".writeOne");
+const check = document.querySelectorAll(".check");
+const rule = document.querySelectorAll(".rule");
 const cancel = document.querySelectorAll(".cancel");
 const todoLeft = document.querySelector(".todo-left");
 
 
 
+todoLeft.textContent = written.length
 
 function bu() {
 
-  // console.log(screen.value);
-  // const wr = document.querySelector(".writeOne");
-  console.log(writeAll.length);
-  const html = `
-  <hr>
-  <div class="writeOne">
-    <div class="written">
-      <input type="checkbox" name="" id="" style="margin-right: 30px;">
-      <h4>${screen.value}</h4>
-    </div>
-    <h1 class="cancel">&times;</h1>
-  </div>
-    `
-  if (screen.value === "") {
 
-    screen.value = '';
-  }
-  else {
-    writeTodo.insertAdjacentHTML("beforeend", html);
-    screen.value = '';
-  }
+    const html = `
+    
+        <div class="writeOne">
+          <div class="written">
+            <input type="checkbox" name="" class="check" style="margin-right: 30px;">
+            <h4 class="rule">${screen.value}</h4>
+          </div>
+          <h1 class="cancel">&times;</h1>
+        </div>
+    `
+
+    if (screen.value === "") {
+
+        screen.value = '';
+    }
+    else {
+        write.insertAdjacentHTML('afterbegin', html);
+        const written = document.querySelectorAll(".writeOne");
+        todoLeft.textContent = written.length
+        const check = document.querySelectorAll(".check");
+        const rule = document.querySelectorAll(".rule");
+        const cancel = document.querySelectorAll(".cancel");
+        for (const [i, ch] of check.entries()) {
+            ch.addEventListener('click', () => {
+                console.log("workin")
+                rule[i].classList.add("ruleOver")
+            })
+        }
+
+        for (const [i, can] of cancel.entries()) {
+            can.addEventListener("click", () => {
+                written[i].innerHTML = '';
+                written[i].classList.remove("writeOne");
+            })
+        }
+        screen.value = '';
+    }
 
 }
 
+function ty() {
+    // console.log("Typing")
+    screen.placeholder = 'Currently typing';
+}
 
+function remove() {
+    for (const [i, can] of cancel.entries()) {
+        can.addEventListener("click", () => {
+            written[i].innerHTML = '';
+            written[i].classList.remove("writeOne");
+        })
+    }
+}
+remove();
